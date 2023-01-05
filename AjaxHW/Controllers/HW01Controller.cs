f3b240331e7555a39e6a640b0dbdf0072a78fc09
Gary.Tsai <az17092145@gmail.com>
2023-01-05 18:44:05 +0800

    加入專案檔案。

---
+++ AjaxHW/Controllers/HW01Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxHW.Controllers
+{
+    public class HW01Controller : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+    }
+}
