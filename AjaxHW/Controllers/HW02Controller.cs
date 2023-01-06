aac4faef8e5aeec4ddb3f171e1c7a837d1bb30a2
Gary.Tsai <az17092145@gmail.com>
2023-01-06 19:25:21 +0800

    1/6 HW02

---
+++ AjaxHW/Controllers/HW02Controller.cs
@@ -0,0 +1,27 @@
+﻿using AjaxHW.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace AjaxHW.Controllers
+{
+    public class HW02Controller : Controller
+    {
+        public readonly DemoContext _context;
+        public HW02Controller(DemoContext context) { 
+        
+        _context = context;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult check(string? name)
+        {
+            var ch = _context.Members.Where(x => x.Name.Equals(name)).ToList();
+            if (ch.Count != 0) {
+                return Content("false", "text/plain", Encoding.UTF8);
+            }
+            return Content("", "text/plain", Encoding.UTF8);
+        }
+    }
+}
