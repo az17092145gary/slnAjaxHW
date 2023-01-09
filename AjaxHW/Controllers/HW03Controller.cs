bc343ab8b092b2bb4e4000bfee8c8f4e2be2f133
Gary.Tsai <az17092145@gmail.com>
2023-01-09 18:55:32 +0800

    HW03

---
+++ AjaxHW/Controllers/HW03Controller.cs
@@ -0,0 +1,36 @@
+﻿using AjaxHW.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxHW.Controllers
+{
+    public class HW03Controller : Controller
+    {
+        public readonly DemoContext _context;
+        public HW03Controller(DemoContext context) 
+        {
+        _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult City()
+        {
+            var citys = _context.Addresses.Select(c => new
+            {
+                c.City
+            }).Distinct().OrderBy(a => a.City);
+            return Json(citys);
+        }
+        public IActionResult Site(string city)
+        {
+            var sites = _context.Addresses.Where(c => c.City == city).Select(c => c.SiteId).Distinct();
+            return Json(sites);
+        }
+        public IActionResult Road(string site)
+        {
+            var roads = _context.Addresses.Where(c => c.SiteId == site).Select(c => c.Road).Distinct();
+            return Json(roads);
+        }
+    }
+}
